6e3cb342d8ee09f2d9c008f1e8b28ac25ee7b216
v_hxin <v_hxin@digitalgd.com.cn>
2021-10-15 15:16:07 +0800

    [+]872 leaf similar tress

--- notes/Algorithm/utils/main.cpp
+++ notes/Algorithm/utils/main.cpp
@@ -14,26 +14,31 @@ struct TreeNode {
 //test example: 
 // int arr[] = { 8,3,10,1,6, -1,14,-1,-1,4,7,13 };
 // int arr[] = { 1, -1, 2, -1, 0, 3};
-TreeNode* arr2BinaryTreeBF(int[] arr) {
-  int len = sizeof(arr)/sizeof(int);
-  queue<TreeNode*> nodeQueue;
-  TreeNode* root = new TreeNode(arr[0]);
-  nodeQueue.push(root);
-  TreeNode* curr;
-  for(int i = 1; i < len; i+=2) {
-    curr = nodeQueue.front();
-    nodeQueue.pop();
+TreeNode* arr2BinaryTreeBF(int arr[], int len) {
+    //int len = sizeof(arr) / sizeof(int);
+    //int len = *(&arr + 1) - arr;
+    if (len == 0) return new TreeNode();
 
-    if(arr[i] > -1) {
-      TreeNode* left = new TreeNode(arr[i]);
-      curr->left = left;
-      nodeQueue.push(left);
+    TreeNode* root = new TreeNode(arr[0]);
+    if (len == 1) return root;
+
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+    TreeNode* curr;
+    for (int i = 1; i < len; i += 2) {
+        curr = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (arr[i] != NULL) {
+            TreeNode* left = new TreeNode(arr[i]);
+            curr->left = left;
+            nodeQueue.push(left);
+        }
+        if (i + 1 < len && arr[i + 1] != NULL) {
+            TreeNode* right = new TreeNode(arr[i + 1]);
+            curr->right = right;
+            nodeQueue.push(right);
+        }
     }
-    if(i + 1 < len && arr[i + 1] > -1) {
-      TreeNode* right = new TreeNode(arr[i+1]);
-      curr-> right = right;
-      nodeQueue.push(right);
-    }
-  }
-  return root;
+    return root;
 }
